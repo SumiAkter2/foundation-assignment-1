98bd422a9bcb5e7ea60b3334d852f2e2415e620a
SumiAkter2 <akteralisha36@gmail.com>
2026-09-02 09:36:23 +0600

    added all ans to answer.js

--- answer.js
+++ answer.js
@@ -61,3 +61,25 @@ function getCngFare(distance, isNight = false, waitingMinutes) {
   }
   return fare;
 }
+
+//question-5 :
+const getChaseVerdict = (target, scored, ballsLeft) => {
+  let runsNeeded = target - scored;
+  let requiredRate;
+  let verdict;
+  if (runsNeeded <= 0) {
+    return "Won";
+  } else if (ballsLeft <= 0) {
+    return "Lost";
+  } else {
+    requiredRate = (runsNeeded / ballsLeft) * 6;
+    if (requiredRate <= 6) {
+      verdict = "Comfortable";
+    } else if (requiredRate > 6 && requiredRate <= 12) {
+      verdict = "Tough";
+    } else {
+      verdict = "Almost Impossible";
+    }
+  }
+  return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
+};
